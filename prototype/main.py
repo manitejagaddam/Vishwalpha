4d7e6f2135dc75870680043caf12626b52bf4686
Maniteja_Gaddam <happyfamily110705@gmail.com>
2025-09-30 14:12:18 +0530

    main file written

---
+++ prototype/main.py
@@ -0,0 +1,16 @@
+from .models.pydantic import ExamAnalysis
+from .OCR.tesseract import extract_text_from_image
+from .OCR.tesseract import extract_text_from_pdf
+from .LLM.openai import analyze_with_llm
+
+def analyze_exam(input_path: str, is_pdf=True) -> ExamAnalysis:
+    if is_pdf:
+        text = extract_text_from_pdf(input_path)
+    else:
+        text = extract_text_from_image(input_path)
+    analysis = analyze_with_llm(text)
+    return analysis
+
+
+if __name__ == "__main__":
+    input_file = 
